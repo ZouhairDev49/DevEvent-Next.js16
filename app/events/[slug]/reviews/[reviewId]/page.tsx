f71c261dab0b53b27880c5049bc0b4a8c1d53679
ZouhairDev49 <zouhaireddoubaji497@gmail.com>
2026-02-09 18:44:31 +0100

    Learn custom NotFound Page

--- app/events/[slug]/reviews/[reviewId]/page.tsx
+++ app/events/[slug]/reviews/[reviewId]/page.tsx
@@ -1,9 +1,13 @@
+import NotFound from "./not-found";
 
 export default async function page({params}:{
   params : Promise<{reviewId: string,slug: string}>
 }) {
   const reviewId = (await params).reviewId;
   const eventId = (await params).slug;
+  if(parseInt(reviewId) > 1001){
+    return <NotFound/>;
+  }
   return (
     <>
       <h1>Review {reviewId} Pour Event {eventId} </h1>
