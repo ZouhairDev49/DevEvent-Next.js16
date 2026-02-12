0d670aa429e9bfb7c602e77b835de6fcc644a1a7
ZouhairDev49 <zouhaireddoubaji497@gmail.com>
2026-02-12 18:46:12 +0100

    Error handling

--- app/events/[slug]/reviews/[reviewId]/page.tsx
+++ app/events/[slug]/reviews/[reviewId]/page.tsx
@@ -1,8 +1,15 @@
 import NotFound from "./not-found";
 
+function getRandomInt(count: number){
+  return Math.floor(Math.random() * count)
+}
 export default async function page({params}:{
   params : Promise<{reviewId: string,slug: string}>
 }) {
+  const random = getRandomInt(2);
+  if (random === 1){
+    throw new Error('Errore loading review');
+  }
   const reviewId = (await params).reviewId;
   const eventId = (await params).slug;
   if(parseInt(reviewId) > 1001){
